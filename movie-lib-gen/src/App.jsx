a227aa10e47f3365cec33dd871d811c58fff3871
Vedant Khullar <vedantkhullar@gmail.com>
2025-04-13 14:28:31 +0530

    Completed movie lib gen

--- movie-lib-gen/src/App.jsx
+++ movie-lib-gen/src/App.jsx
@@ -4,7 +4,7 @@ import Search from './components/Search';
 import { useEffect } from 'react';
 import MovieCard from './components/MovieCard';
 import {useDebounce} from 'react-use'
-import { updateSearchTerm } from './appwrite';
+import { getTrendingMovies, updateSearchTerm } from './appwrite';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -12,9 +12,10 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [debounceSearchTerm, setDebounceSearchTerm] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState('');
 
 
-  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm])
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm]);
   
 
 
@@ -27,6 +28,7 @@ function App() {
       Authorization: `Bearer ${API_KEY}`
     }
   }
+
   const fetchMovies = async (query) => {
 
     setErrorMessage('');
@@ -51,9 +53,23 @@ function App() {
     }
   }
 
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch(error){
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debounceSearchTerm);
-  }, [debounceSearchTerm])
+  }, [debounceSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, [])
+   
   return (
     <main>
       <div className="pattern">
@@ -65,8 +81,26 @@ function App() {
             </h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
+
+          {
+            trendingMovies.length > 0 && (
+              <section className='trending'>
+                <h2>Trending Movies</h2>
+                <ul>
+                  {
+                  trendingMovies.map((movie, index) => (
+                    <li key={movie.$id}>
+                      <p>{index + 1}</p>
+                      <img src={movie.poster_url} alt={movie.title} />
+                    </li>
+                  ))}
+                </ul>
+
+              </section>
+            )
+          }
           <section className='all-movies'>
-            <h2 className='mt-[40px]'>All Movies</h2>
+            <h2 >All Movies</h2>
             {isLoading ? (
               <p className='text-white'>Loading...</p>
             ): errorMessage ? (
--- movie-lib-gen/src/appwrite.js
+++ movie-lib-gen/src/appwrite.js
@@ -30,3 +30,16 @@ export const updateSearchTerm = async (searchTerm, movie) => {
     console.error(error);
   }
 };
+
+export const getTrendingMovies = async() => {
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count')
+        ])
+
+        return result.documents;
+    } catch(error){
+        console.error(error);
+    }
+}
